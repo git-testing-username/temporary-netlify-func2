245ef80494b3728891f06807652dd51bffab64b4
Basho lim <basholim@gmail.com>
2022-06-08 22:38:28 +0800

    test3

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -5,7 +5,7 @@ const fetchData = async () => {
     // const { data } = await axios.get("/.netlify/functions/1-hello");
     const { data } = await axios.get("/api/1-hello");
 
-    result.textContent = data;
+    result.textContent = data + "test";
   } catch (error) {
     // console.log(error.response)
     result.textContent = error.response.data;
